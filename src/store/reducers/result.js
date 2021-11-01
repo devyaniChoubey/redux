0919792f2bd2af6980800f38a6476019ee75defe
devyani choubey <devyanichoubey16@gmail.com>
2021-11-01 13:59:02 +0530

    first commit

---
+++ src/store/reducers/result.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actions';
+
+const initialState ={
+  counter : 0,
+  results : []
+}
+
+const reducer = (state, action ) => {
+
+    switch(action.type){
+        case actionTypes.STORE_RESULT:
+            return{
+                ...state,
+                results: state.results.concat({id : new Date() , value : action.result})
+            }
+        case actionTypes.DELETE_RESULT:
+            const updatedArray = state.results.filter(result => result.id !== action.resultId)
+            return{
+                ...state,
+                results : updatedArray
+            }
+
+
+    }
+    return state;
+
+} 
+export default reducer;
